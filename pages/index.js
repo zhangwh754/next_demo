c7e5c40c6594dee21d473f023d343d0e652bd591
张文皓 <zhangwh754@163.com>
2023-11-07 16:41:17 +0800

    add css get prettier page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import Link from "next/link";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <h2>Hello World</h2>
-      <Link href="/posts/first-post">link</Link>
-    </div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[知耻而后勇]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://www.nextjs.cn/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   );
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <div className="container">
-      <h1>First Post</h1>
-      <Link href="/">back to Home</Link>
-    </div>
+    <Layout>
+      <Head>
+        <title>first post</title>
+      </Head>
+
+      <div className="container">
+        <h1>First Post</h1>
+        <Link href="/">back to Home</Link>
+      </div>
+    </Layout>
   );
 }
