a4cf96c90d3c9ec70b3c91e703673e227f6f2387
张文皓 <zhangwh754@163.com>
2023-11-08 09:47:54 +0800

    getStaticProps读取本地md文件的front-matter显示在页面上

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,43 @@
 import Head from "next/head";
+import { getSortedPostsData } from "../lib/post";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+export default function Home({ sortedData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[知耻而后勇]</p>
+        <p>知耻而后勇</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://www.nextjs.cn/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {sortedData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              {/* <br />
+              {id} */}
+              <br />
+              {date.slice(0, 10)}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const sortedData = getSortedPostsData();
+
+  return {
+    props: { sortedData },
+  };
 }
---
+++ posts/vue study.md
@@ -0,0 +1,6 @@
+---
+title: 'Vue学习'
+date: '2023-11-08 08:00:00'
+---
+
+Vue有version2和version3两个大版本
